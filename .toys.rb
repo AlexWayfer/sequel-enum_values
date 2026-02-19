fbe5ad6e1814d0a9377153cb434c680cdcf64454
Alexander Popov <alex.wayfer@gmail.com>
2026-02-19 21:12:16 +0300

    Specify version file for `gem_toys`

--- .toys.rb
+++ .toys.rb
@@ -3,6 +3,7 @@
 include :bundler, static: true
 
 require 'gem_toys'
-expand GemToys::Template
+expand GemToys::Template,
+	version_file_path: 'lib/sequel/plugins/enum_values/version.rb'
 
 alias_tool :g, :gem
